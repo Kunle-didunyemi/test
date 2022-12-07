53be90de1f820aa0611f5a0305a89e47ec2a8486
Senator kay <senatorkay2@gmail.com>
2022-12-08 00:00:50 +0100

    add

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         adekunle Learn React
         </a>
       </header>
     </div>
